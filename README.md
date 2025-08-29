ea5086738ed3481ed07ba1a71f5ecdbb3bb96f51
Iris <irisaunav@gmail.com>
2025-08-30 00:32:35 +0200

    Add project files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# ClockAlert
+## Tabla de contenidos
+
+- [Acerca de este repositorio](#Acerca-de-este-repositorio)
+- [Uso](#Uso)
+- [Por hacer](#Por-hacer)
+
+## Acerca de este repositorio
+
+Proporciona alertas en el juego cuando se hace de noche y te encuentras lejos de la granja.  
+
+Programa creado con **C#** y haciendo uso de **SMAPI**.  
+Probado con **Windows 10** y la versión **4.3.2** de **SMAPI**.  
+
+❗ Requiere tener SMAPI instalado.
+
+## Uso
+
+1. El mod como tal está en el directorio **dist**, el código fuente se encuentra en **src**
+2. Copia el directorio **ClockAlert** que se encuentra dentro de **dist** en la la ruta `Stardew Valley\Mods`
+
+## Por hacer
+
+- Probar su uso en multijugador
+- Traducir las líneas al inglés
---
+++ src/ClockAlert/ClockAlert/ModEntry.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using xTile.Dimensions;
+
+namespace ClockAlert
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.TimeChanged += this.ShowAlert;
+        }
+
+        private static int GetTime(object? sender, TimeChangedEventArgs e)
+        {
+            // get current time
+            return Game1.timeOfDay;
+        }
+
+        private static string MessageTime(object? sender, TimeChangedEventArgs e)
+        {
+            int time = GetTime(sender, e);
+            string location = Game1.currentLocation.Name;
+
+            // if player is away from farm
+            if (location != "farm")
+            {
+                // time messages
+                switch (time)
+                {
+                    case 2200:
+                        return "Ya son las 10 de la noche.";
+                    case 2300:
+                        return "Son las 11 de la noche.";
+                    case 2400:
+                        return "Cuidado. Son las 12 de la noche, vete a casa.";
+                    default:
+                        break;
+                }
+            }
+
+            return "";
+        }
+
+        private void ShowAlert(object? sender, TimeChangedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            if (MessageTime(sender, e) != "")
+            {
+                // add message to game
+                Game1.addHUDMessage(new HUDMessage($"{MessageTime(sender, e)}", 2));
+                // and play sound
+                Game1.playSound("detector");
+            }
+        }
+    }
+}
